32b6234f1f493e320feedf358481c2bd234f395f
mfallon1 <mfallon1@my.wctc.edu>
2019-11-05 03:11:39 +0000

    1105 class changes

--- Northwind-Console/Models/Category.cs
+++ Northwind-Console/Models/Category.cs
@@ -8,6 +8,8 @@ namespace NorthwindConsole.Models
         public int CategoryId { get; set; }
         [Required(ErrorMessage = "YO - Enter the name!")]
         public string CategoryName { get; set; }
+
+ //       [StringLength(50)]   // limits the description to 50 char
         public string Description { get; set; }
 
         public virtual List<Product> Products { get; set; }
--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Validation;
 using System.Linq;
 using NLog;
 using NorthwindConsole.Models;
@@ -36,7 +37,7 @@ namespace NorthwindConsole
                         {
                             Console.WriteLine($"{item.CategoryName} - {item.Description}");
                         }
-                    } 
+                    }
                     else if (choice == "2")
                     {
                         Category category = new Category();
@@ -45,13 +46,21 @@ namespace NorthwindConsole
                         Console.WriteLine("Enter the Category Description:");
                         category.Description = Console.ReadLine();
 
-                        ValidationContext context = new ValidationContext(category, null, null);
+                        ValidationContext context = new ValidationContext(category, null, null); // what do I want to validate? = category put category in our context
                         List<ValidationResult> results = new List<ValidationResult>();
 
-                        var isValid = Validator.TryValidateObject(category, context, results, true);
+                        var isValid = Validator.TryValidateObject(category, context, results, true); // validate category and return it to results = bool
                         if (isValid)
                         {
                             var db = new NorthwindContext();
+                            //                            db.Categories.Add(category); //added in class
+                            //                            var erro = db.GetValidationErrors(); //added in class
+                            //                            if (erro.Any()) //added in class
+                            //                            {
+                            // what to do if there is an error dont save to db do something else
+                            //                            }
+
+                            //                            db.SaveChanges(); // added in class
                             // check for unique name
                             if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
                             {
@@ -73,41 +82,46 @@ namespace NorthwindConsole
                             }
                         }
                     } else if (choice == "3")
-                    {
-                        var db = new NorthwindContext();
-                        var query = db.Categories.OrderBy(p => p.CategoryId);
+                        {
+                            var db = new NorthwindContext();
+                            var query = db.Categories.OrderBy(p => p.CategoryId);
 
-                        Console.WriteLine("Select the category whose products you want to display:");
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
-                        }
-                        int id = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        logger.Info($"CategoryId {id} selected");
-                        Category category = db.Categories.FirstOrDefault(c => c.CategoryId == id);
-                        Console.WriteLine($"{category.CategoryName} - {category.Description}");
-                        foreach(Product p in category.Products)
-                        {
-                            Console.WriteLine(p.ProductName);
-                        }
-                    }
-                    else if (choice == "4")
-                    {
-                        var db = new NorthwindContext();
-                        var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
-                        foreach(var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryName}");
-                            foreach(Product p in item.Products)
+                            Console.WriteLine("Select the category whose products you want to display:");
+                            foreach (var item in query)
                             {
-                                Console.WriteLine($"\t{p.ProductName}");
+                                Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
+                            }
+                            int id = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            logger.Info($"CategoryId {id} selected");
+                            Category category = db.Categories.FirstOrDefault(c => c.CategoryId == id);
+                            Console.WriteLine($"{category.CategoryName} - {category.Description}");
+                            foreach (Product p in category.Products) // products enumerated in the category bcause of the list
+                            {
+                                Console.WriteLine(p.ProductName);
                             }
                         }
-                    }
-                    Console.WriteLine();
+                        else if (choice == "4")
+                        {
+                            var db = new NorthwindContext();
+                            var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId); // dont do .ToList() after because we can add to it with where etc.
+ // doesn't work                        query.Where(c => c.Description.Contains("xxxx")); // added in class
+                        foreach (var item in query)
+                            {
+                                Console.WriteLine($"{item.CategoryName}");
+                                foreach (Product p in item.Products)
+                                {
+                                    Console.WriteLine($"\t{p.ProductName}");
+                                }
+                            }
+                        }
+                        Console.WriteLine();
 
-                } while (choice.ToLower() != "q");
+                    } while (choice.ToLower() != "q") ;
+                }
+                catch (DbEntityValidationException) // code added in class
+            {
+                // in github
             }
             catch (Exception ex)
             {
