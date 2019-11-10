ed965ad4aa8ce58aca5b04c7c42a46593e201390
mfallon1 <mfallon1@my.wctc.edu>
2019-11-10 21:42:08 +0000

    All Validation completed Final commit

--- Northwind-Console/Program.cs
+++ Northwind-Console/Program.cs
@@ -32,10 +32,10 @@ namespace NorthwindConsole
                         var db = new NorthwindContext();
                         var query = db.Categories.OrderBy(p => p.CategoryName);
 
-                        Console.WriteLine($"{query.Count()} records returned");
+                        Console.WriteLine($"There are {query.Count()} Categories:");
                         foreach (var item in query)
                         {
-                            Console.WriteLine($"{item.CategoryName} - {item.Description}");
+                            Console.WriteLine($"\t{item.CategoryName} - {item.Description}");
                         }
                     }
                     else if (choice == "2")
@@ -53,14 +53,13 @@ namespace NorthwindConsole
                         if (isValid)
                         {
                             var db = new NorthwindContext();
-                            db.Categories.Add(category); //added in class
-                            var erro = db.GetValidationErrors(); //added in class
+                            db.Categories.Add(category); 
+                            var erro = db.GetValidationErrors(); 
                             if (erro.Any()) //added in class
                             {
                                 Console.WriteLine(erro);
                             }
 
-                           // db.SaveChanges(); // added in class
                             // check for unique name
                             if (db.Categories.Any(c => c.CategoryName == category.CategoryName))
                             {
@@ -71,17 +70,15 @@ namespace NorthwindConsole
                             else
                             {
                                 logger.Info("Validation passed");
-                                db.SaveChanges(); // added in class
-                                // TODO: save category to db
+                                db.SaveChanges(); 
                             }
                         }
                         if (!isValid)
                         {
                             foreach (var result in results)
                             {
-       //                         logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
-                                logger.Error($"{result.ErrorMessage}");
-                                Console.WriteLine($"ERROR: {result.ErrorMessage}");
+                                 logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
+                                 Console.WriteLine($"ERROR: {result.ErrorMessage}");
                             }
                         }
 
@@ -90,7 +87,7 @@ namespace NorthwindConsole
                             var db = new NorthwindContext();
                             var query = db.Categories.OrderBy(p => p.CategoryId);
 
-                            Console.WriteLine("Select the category whose products you want to display:");
+                            Console.WriteLine("Select the category you want to display:");
                             foreach (var item in query)
                             {
                                 Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
@@ -98,40 +95,63 @@ namespace NorthwindConsole
                             int id = int.Parse(Console.ReadLine());
                             Console.Clear();
                             logger.Info($"CategoryId {id} selected");
+                        try
+                        {
                             Category category = db.Categories.FirstOrDefault(c => c.CategoryId == id);
                             Console.WriteLine($"{category.CategoryName} - {category.Description}");
+
+                            GetProductCount(id); // display the product count first
+
                             foreach (Product p in category.Products) // products enumerated in the category bcause of the list
                             {
-                                Console.WriteLine(p.ProductName);
+                                Console.WriteLine($"\t{p.ProductName}");
                             }
+                        }
+                        catch
+                        {
+                            logger.Info("Error in Selection"); 
+                            Console.WriteLine($"** Error - Try again");
+                        }
                         }
                         else if (choice == "4")
                         {
                             var db = new NorthwindContext();
                             var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId); // dont do .ToList() after because we can add to it with where etc.
- // doesn't work                        query.Where(c => c.Description.Contains("xxxx")); // added in class
+
                         foreach (var item in query)
                             {
-                                Console.WriteLine($"{item.CategoryName}");
-                                foreach (Product p in item.Products)
-                                {
+                                Console.WriteLine($"\n{item.CategoryName}:");
+                                GetProductCount(item.CategoryId); // display the product count first
+
+                               foreach (Product p in item.Products)
+                               {
                                     Console.WriteLine($"\t{p.ProductName}");
-                                }
+                               }
                             }
                         }
                         Console.WriteLine();
 
                     } while (choice.ToLower() != "q") ;
                 }
-                catch (DbEntityValidationException) // code added in class
+                catch (DbEntityValidationException)
             {
-                // in github
+
             }
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
+                Console.WriteLine($"Error {ex.Message} Press Enter to close");
+                Console.ReadLine();
             }
             logger.Info("Program ended");
+        }
+
+        private static void GetProductCount(int cId)  // get product count for Category
+        {
+            var db = new NorthwindContext();
+            var cquery = db.Products.Where(p => p.CategoryId == cId).Count();
+            Console.WriteLine($"\n** {cquery} Product(s) returned\n");
+
         }
     }
 }
